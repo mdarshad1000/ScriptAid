ad6d38610a522d968435cfd1c1a0bc2b5d0ab009
Mohammad Arshad <mdarshad1000@gmail.com>
2023-01-10 21:17:27 +0530

    Push Flask App

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import openai
+import os
+from flask_wtf.csrf import CSRFProtect
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+app = Flask(__name__)
+
+app.config.update(
+    SECRET_KEY=os.environ.get("FLASK_SECRET"),
+    SESSION_COOKIE_HTTPONLY=True,
+    REMEMBER_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE="Strict",
+)
+
+csrf = CSRFProtect() 
+csrf.init_app(app)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/suggest', methods=['POST', 'GET'])
+def suggest():
+
+    style = request.json["style"] if request.json["style"] else ""
+
+    response = openai.Completion.create(
+        engine = "text-davinci-003",
+        prompt = f"This is a {style} {request.json['type']} about {request.json['topic']}:\n\n{request.json['content']}",
+        max_tokens=15,
+        temperature=0.7,
+        top_p=1, 
+    )
+    print(response)
+    return {"suggestion": response["choices"][0]["text"]}
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=7383)
---
+++ static/script.js
@@ -0,0 +1,74 @@
+// Define variables
+let suggest_endpoint = document.getElementsByName("suggest_endpoint")[0].content;
+let csrf = document.getElementsByName("csrf-token")[0].content;
+let timeout;
+let suggesting = false;
+const contentDiv = document.getElementById("content");
+const typeSelect = document.getElementById("type");
+const topicInput = document.getElementById("topic");
+const styleInput = document.getElementById("style");
+
+// Send a request to the suggest endpoint after wait
+const sendRequest = (wait = 1000) => {
+    clearTimeout(timeout);
+    if (contentDiv.textContent !== "") {
+        timeout = setTimeout(() => {
+            fetch(suggest_endpoint, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrf,
+                },
+                body: JSON.stringify({
+                    type: typeSelect.value,
+                    topic: topicInput.value,
+                    style: styleInput.value,
+                    content: contentDiv.innerText,
+                }),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    // Add the suggestion text in grey color after the existing text in the content div
+                    const suggestion = document.createElement("span");
+                    suggestion.id = "suggestion";
+                    suggestion.classList.add("text-secondary");
+                    suggestion.innerText = data.suggestion;
+                    contentDiv.appendChild(suggestion);
+                    suggesting = true;
+                });
+        }, wait);
+    }
+};
+
+// Send a request on new input
+contentDiv.addEventListener("input", (event) => {
+    sendRequest(600);
+});
+
+contentDiv.addEventListener("keydown", (event) => {
+    // If the "Tab" key is pressed, turn the new text black and move the cursor to the end
+    if ((event.key === "Tab"  || event.key === "Enter") && suggesting === true) {
+        event.preventDefault();
+        const suggestions = contentDiv.querySelectorAll("[id^=suggestion]");
+        suggestions.forEach((suggestion) => {
+            suggestion.id = "accepted";
+            suggestion.classList.remove("text-secondary");
+        });
+        suggesting = false;
+        // Move the cursor to the end of the text
+        const range = document.createRange();
+        range.selectNodeContents(contentDiv);
+        range.collapse(false);
+        const selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+        // Send a new request after the previous response has been accepted
+        sendRequest(200);
+    } else if (suggesting === true) {
+        // If any other key is pressed, remove the new text and don't move the cursor
+        event.preventDefault();
+        const suggestions = contentDiv.querySelectorAll("[id^=suggestion]");
+        suggestions.forEach((suggestion) => suggestion.remove());
+        suggesting = false;
+    }
+});
